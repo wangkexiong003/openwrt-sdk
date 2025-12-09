6f02ebe0e8ae22b03364470a0c4822c9e6bd2ea1
wangkexiong <wangkexiong@gmail.com>
2025-12-09 16:58:35 +0800

    check vhdx for x86

--- scripts/image_artifacts.sh
+++ scripts/image_artifacts.sh
@@ -4,7 +4,7 @@ BUILD_VERSION="$1"
 mkdir -p artifacts
 
 if [ -d openwrt ]; then
-  #find openwrt/bin -type f -name "*.img.gz" -exec sh -c 'dirname "$1"' _ {} \; | sort -u | xargs ls -al
+  find openwrt/bin -type f -name "*.img.gz" -exec sh -c 'dirname "$1"' _ {} \; | sort -u | xargs ls -al
 
   ## for x86_64
   find openwrt/bin -type f -name "*squashfs-combined-efi.img.gz" 2>/dev/null -exec bash -c '
