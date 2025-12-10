36dd4537681a5cad51df880fe63f4140a806b8bd
wangkexiong <wangkexiong@gmail.com>
2025-12-10 14:38:27 +0800

    add repo index

--- scripts/image_artifacts.sh
+++ scripts/image_artifacts.sh
@@ -4,10 +4,13 @@ BUILD_VERSION="$1"
 mkdir -p artifacts
 
 if [ -d openwrt ]; then
-  find openwrt/bin -type f -name "*.img.gz" -exec sh -c 'dirname "$1"' _ {} \; | sort -u | xargs ls -al
+  #find openwrt/bin -type f -name "*.img.gz" -exec sh -c 'dirname "$1"' _ {} \; | sort -u | xargs ls -al
 
   ## for x86_64
   find openwrt/bin -type f -name "*squashfs-combined-efi.img.gz" 2>/dev/null -exec bash -c '
+    cp -v "$1" artifacts/${BUILD_VERSION}-$(basename "$1")
+  ' _ {} \;
+  find openwrt/bin -type f -name "*squashfs-combined-efi.vhdx" 2>/dev/null -exec bash -c '
     cp -v "$1" artifacts/${BUILD_VERSION}-$(basename "$1")
   ' _ {} \;
 fi
